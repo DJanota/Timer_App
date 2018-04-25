aa5e6739221bb57f7c37b24274fbca987c8d664b
Dawid Janota <6dawid.janota@wp.pl>
2018-04-25 19:20:43 +0200

    git-ES6-15.6

--- script.js
+++ script.js
@@ -6,16 +6,7 @@ class Stopwatch {
         this.print(this.times);
     }
 
-    static pad0(value) {
-        let result = value.toString();
-        if (result.length < 2) {
-            result = '0' + result;
-        }
-        return result;
-    }
-
-
-    reset() {
+    reset() { // resets the stopwatch
         this.times = {
             minutes: 0,
             seconds: 0,
@@ -23,52 +14,69 @@ class Stopwatch {
         };
     }
 
-    start() {
-		if (!this.running) {
-			this.running = true;
-			this.watch = setInterval(() => this.step(), 10);
-		}
-	}
+    resetTimer() {
+        this.stop();
+        this.reset();
+        this.print();
+    }
 
-	step() {
-		if (!this.running) return;
-		this.calculate();
-		this.print();
-	}
-
-	calculate() {
-		this.times.miliseconds += 1;
-		if (this.times.miliseconds >= 100) {
-			this.times.seconds += 1;
-			this.times.miliseconds = 0;
-		}
-		if (this.times.seconds >= 60) {
-			this.times.minutes += 1;
-			this.times.seconds = 0;
-		}
-	}
-
-	stop() {
-		this.running = false;
-		clearInterval(this.watch);
-	}
-
-
-    print() {
+    print() { // inserts text under 'display' attribute (text created with the below 'format' method)
         this.display.innerText = this.format(this.times);
     }
 
-    format(times) {
+    format(times) { 
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
 
+    start() {
+        if (!this.running) { // check if the timer is not running
+            this.running = true; // if so, turn it on
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    step() { // checks if the timer is working
+        if (!this.running) return;
+        this.calculate(); // if so, calculate miliseconds, secs and mins (below)
+        this.print(); // prints the result of the calculation
+    }
+
+    calculate() {
+        this.times.miliseconds += 1; // we are constantly adding a milisecond
+        if (this.times.miliseconds >= 100) { // 1000 / 10 = 100 (one second has 1000 miliseconds; in the 'start' method we set 10ms interval); // if the number of miliseconds reaches 100, we add one second
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) { // if the number of seconds reaches 60, we add one minute
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
 }
 
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
+// pad0 function adds '0' to one-digit numbers
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
 
+const stopwatch = new Stopwatch( // 'Stopwatch' class instance
+    document.querySelector('.stopwatch'));
+
+// click buttons methods
 var startButton = document.getElementById('start');
 startButton.addEventListener('click', () => stopwatch.start());
 
 var stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', () => stopwatch.stop());
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.resetTimer());
