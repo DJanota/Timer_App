17abbbc381060ee8673b565fe85de5ef4e120aff
Dawid Janota <6dawid.janota@wp.pl>
2019-09-25 09:42:43 +0200

    ES6

--- script-compiled.js
+++ script-compiled.js
@@ -1,8 +1,27 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = function () {
+    function defineProperties(target, props) {
+        for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+        }
+    }
+    return function (Constructor, protoProps, staticProps) {
+        if (protoProps) defineProperties(Constructor.prototype, protoProps);
+        if (staticProps) defineProperties(Constructor, staticProps);
+        return Constructor;
+    };
+}();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
 
 var Stopwatch = function () {
     function Stopwatch(display) {
@@ -42,7 +61,7 @@ var Stopwatch = function () {
         value: function format(times) {
             return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
 
-            // pad0 function adds '0' to one-digit numbers
+            // pad function adds '0' to one-digit numbers
             function pad0(value) {
                 var result = value.toString();
                 if (result.length < 2) {
@@ -99,7 +118,7 @@ var Stopwatch = function () {
 }();
 
 var stopwatch = new Stopwatch( // 'Stopwatch' class instance
-document.querySelector('.stopwatch'));
+    document.querySelector('.stopwatch'));
 
 // click buttons methods
 var startButton = document.getElementById('start');
